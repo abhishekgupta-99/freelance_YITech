1f22f5dce6dec72647611d600d124f39a157a9be
abhishekgupta-99 <abhishekgupta97023@gmail.com>
2020-12-05 21:56:27 +0530

    Initial Commit

--- app/src/main/java/com/freelance/school_attendance/AppIntro.java
+++ app/src/main/java/com/freelance/school_attendance/AppIntro.java
@@ -1,12 +1,34 @@
 package com.freelance.school_attendance;
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
-public class AppIntro extends AppCompatActivity {
+import agency.tango.materialintroscreen.MaterialIntroActivity;
+import agency.tango.materialintroscreen.MessageButtonBehaviour;
+import agency.tango.materialintroscreen.SlideFragmentBuilder;
+
+public class AppIntro extends MaterialIntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_app_intro);
+     //   setContentView(R.layout.activity_app_intro);
+
+        addSlide(new SlideFragmentBuilder()
+                        .backgroundColor(R.color.colorPrimary)
+                        .buttonsColor(R.color.colorAccent)
+                     //   .possiblePermissions(new String[]{Manifest.permission.CALL_PHONE})
+                      //  .neededPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION})
+                        .image(R.drawable.ic_attendance)
+                        .title("FreeLance")
+                        .description("A school attendance management app")
+                        .build(),
+                new MessageButtonBehaviour(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showMessage("We provide solutions to make you love your work");
+                    }
+                }, "Google SignIn"));
     }
 }
