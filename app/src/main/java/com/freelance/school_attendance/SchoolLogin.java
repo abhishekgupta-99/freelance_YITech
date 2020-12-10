648788e338f7fe65322e9b1cb1bbeb407dd3352e
Pranjal <pranjalpchaudhari99@gmail.com>
2020-12-11 00:03:32 +0530

    UI changes

--- app/src/main/java/com/freelance/school_attendance/SchoolLogin.java
+++ app/src/main/java/com/freelance/school_attendance/SchoolLogin.java
@@ -2,7 +2,9 @@ package com.freelance.school_attendance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SchoolLogin extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class SchoolLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_school_login);
+    }
+
+    public void goToDropDown(View view) {
+        Intent i = new Intent(this,DropDownMenus.class);
+        startActivity(i);
     }
 }
